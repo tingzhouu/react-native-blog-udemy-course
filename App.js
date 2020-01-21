f55392c8279e6302e65b70022ca91f8c0cd5d5de
tingzhouu <limtingzhou@gmail.com>
2020-01-21 19:03:53 +0800

    Initial commit with index screen

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation'; 
+import IndexScreen from './src/screens/IndexScreen';
+
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs',
+  }
+})
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />
+}
